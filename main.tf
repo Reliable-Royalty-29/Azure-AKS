3f3b56b5e94977f03225a17d520b1e4b337d705f
Dheeman Das <84588268+Reliable-Royalty-29@users.noreply.github.com>
2023-05-29 00:35:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "azurerm_resource_group" "aks-rg" {
-  name     = var.AKS
-  location = var.EastUs
+  name     = var.resource_group_name
+  location = var.location
 }
 
 resource "azurerm_role_assignment" "role_acrpull" {
@@ -11,19 +11,19 @@ resource "azurerm_role_assignment" "role_acrpull" {
 }
 
 resource "azurerm_container_registry" "acr" {
-  name                = var.Deva
-  resource_group_name = azurerm_resource_group.aks-rg.AKS
-  location            = var.EastUs
+  name                = var.acr_name
+  resource_group_name = azurerm_resource_group.aks-rg.name
+  location            = var.location
   sku                 = "Standard"
   admin_enabled       = false
 }
 
 resource "azurerm_kubernetes_cluster" "aks" {
-  name                = var.Shiva
+  name                = var.cluster_name
   kubernetes_version  = var.kubernetes_version
-  location            = var.EastUs
-  resource_group_name = azurerm_resource_group.aks-rg.AKS
-  dns_prefix          = var.Shiva
+  location            = var.location
+  resource_group_name = azurerm_resource_group.aks-rg.name
+  dns_prefix          = var.cluster_name
 
   default_node_pool {
     name                = "system"
@@ -40,6 +40,6 @@ resource "azurerm_kubernetes_cluster" "aks" {
 
   network_profile {
     load_balancer_sku = "Standard"
-    network_plugin    = "kubenet" 
+    network_plugin    = "kubenet"
   }
 }
